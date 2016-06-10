1008c2a72c9025dcec46e27d2dc081a38937b9a6
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2016-06-10 17:18:15 -0600

    Make Changes to Kickstart a Travis CI Build.

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ var dstDt, stdDt;
 
 stdDt = new Date(1976, 2, 14, 1, 59, 59, 999);
 console.log(stdDt);
+console.log(stdDt.getTimezoneOffset());
 
 dstDt = new Date(stdDt.getTime() + 1);
 console.log(dstDt);
+console.log(dstDt.getTimezoneOffset());
 
 dstDt = new Date(stdDt.getTime() + 2);
 console.log(dstDt);
+console.log(dstDt.getTimezoneOffset());
