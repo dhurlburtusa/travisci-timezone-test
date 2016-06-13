e17e6345c1c4719dc076473295b7e6b6e766981d
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2016-06-13 12:07:56 -0600

    List More Specific Timezone Info.

--- listtz.sh
+++ listtz.sh
@@ -2,3 +2,23 @@
 
 cd /usr/share/zoneinfo/
 ls -al
+
+ls -al Africa/
+ls -al America/
+ls -al Antarctica/
+ls -al Arctic/
+ls -al Asia/
+ls -al Atlantic/
+ls -al Australia/
+ls -al Brazil/
+ls -al Canada/
+ls -al Chile/
+ls -al Etc/
+ls -al Europe/
+ls -al Indian/
+ls -al Mexico/
+ls -al Pacific/
+ls -al posix/
+ls -al right/
+ls -al SystemV/
+ls -al US/
