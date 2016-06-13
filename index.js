7c5d76f0475956fa6bf7fc1836388b1b5fcfa23a
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2016-06-13 14:06:42 -0600

    Add More Potential DST Times.

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 
 var dstDt, stdDt;
 
+function print(dt) {
+  console.log(dt.getTimezoneOffset());
+  console.log(dt);
+  console.log(dt.toString());
+  console.log(dt.toISOString());
+}
 
 stdDt = new Date(1976, 2, 14, 1, 59, 59, 999);
-console.log(stdDt);
-console.log(stdDt.getTimezoneOffset());
+print(stdDt);
 
 dstDt = new Date(stdDt.getTime() + 1);
-console.log(dstDt);
-console.log(dstDt.getTimezoneOffset());
+print(dstDt);
 
 dstDt = new Date(stdDt.getTime() + 2);
-console.log(dstDt);
-console.log(dstDt.getTimezoneOffset());
+print(dstDt);
+
+
+stdDt = new Date(1976, 2, 21, 1, 59, 59, 999);
+print(stdDt);
+
+dstDt = new Date(stdDt.getTime() + 1);
+print(dstDt);
+
+dstDt = new Date(stdDt.getTime() + 2);
+print(dstDt);
+
+
+stdDt = new Date(1976, 2, 28, 1, 59, 59, 999);
+print(stdDt);
+
+dstDt = new Date(stdDt.getTime() + 1);
+print(dstDt);
+
+dstDt = new Date(stdDt.getTime() + 2);
+print(dstDt);
+
+
+stdDt = new Date(1976, 3, 4, 1, 59, 59, 999);
+print(stdDt);
+
+dstDt = new Date(stdDt.getTime() + 1);
+print(dstDt);
+
+dstDt = new Date(stdDt.getTime() + 2);
+print(dstDt);
