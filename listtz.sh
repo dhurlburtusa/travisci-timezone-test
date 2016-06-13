25357160792d603a421e6e75bd52d0392a9a2d73
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2016-06-13 12:34:04 -0600

    Format Output of listtz.sh script.

--- listtz.sh
+++ listtz.sh
@@ -3,22 +3,60 @@
 cd /usr/share/zoneinfo/
 ls -al
 
+echo
+echo Africa:
 ls -al Africa/
+echo
+echo America:
 ls -al America/
+echo
+echo Antarctica:
 ls -al Antarctica/
+echo
+echo Arctic:
 ls -al Arctic/
+echo
+echo Asia:
 ls -al Asia/
+echo
+echo Atlantic:
 ls -al Atlantic/
+echo
+echo Australia:
 ls -al Australia/
+echo
+echo Brazil:
 ls -al Brazil/
+echo
+echo Canada:
 ls -al Canada/
+echo
+echo Chile:
 ls -al Chile/
+echo
+echo Etc:
 ls -al Etc/
+echo
+echo Europe:
 ls -al Europe/
+echo
+echo Indian:
 ls -al Indian/
+echo
+echo Mexico:
 ls -al Mexico/
+echo
+echo Pacific:
 ls -al Pacific/
+echo
+echo posix:
 ls -al posix/
+echo
+echo right:
 ls -al right/
+echo
+echo SystemV:
 ls -al SystemV/
+echo
+echo US:
 ls -al US/
