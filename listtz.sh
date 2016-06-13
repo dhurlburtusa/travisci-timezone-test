557d02a13ce40d1e4bd121c212dff7aed1cce7ba
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2016-06-13 12:59:36 -0600

    List More Specific America Specific Timezones.

--- listtz.sh
+++ listtz.sh
@@ -6,57 +6,91 @@ ls -al
 echo
 echo Africa:
 ls -al Africa/
+
 echo
 echo America:
 ls -al America/
+
+echo
+echo America/Argentina:
+ls -al America/Argentina
+
+echo
+echo America/Indiana:
+ls -al America/Indiana
+
+echo
+echo America/Kentucky:
+ls -al America/Kentucky
+
+echo
+echo America/North_Dakota:
+ls -al America/North_Dakota
+
 echo
 echo Antarctica:
 ls -al Antarctica/
+
 echo
 echo Arctic:
 ls -al Arctic/
+
 echo
 echo Asia:
 ls -al Asia/
+
 echo
 echo Atlantic:
 ls -al Atlantic/
+
 echo
 echo Australia:
 ls -al Australia/
+
 echo
 echo Brazil:
 ls -al Brazil/
+
 echo
 echo Canada:
 ls -al Canada/
+
 echo
 echo Chile:
 ls -al Chile/
+
 echo
 echo Etc:
 ls -al Etc/
+
 echo
 echo Europe:
 ls -al Europe/
+
 echo
 echo Indian:
 ls -al Indian/
+
 echo
 echo Mexico:
 ls -al Mexico/
+
 echo
 echo Pacific:
 ls -al Pacific/
+
 echo
 echo posix:
 ls -al posix/
+
 echo
 echo right:
 ls -al right/
+
 echo
 echo SystemV:
 ls -al SystemV/
+
 echo
 echo US:
 ls -al US/
