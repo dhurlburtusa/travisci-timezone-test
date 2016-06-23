3fa1e671a7137b40da9adb6928a9f1fa05167504
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2016-06-23 16:21:11 -0600

    Add More Test Dates to See Output of Date#toString

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 var dstDt, stdDt;
 
+function createDt(dateParts) {
+  var dp = dateParts;
+  return new Date(dp[0], dp[1], dp[2], dp[3], dp[4], dp[5], dp[6]);
+}
+
 function print(dt) {
   console.log(dt.getTimezoneOffset());
   console.log(dt);
@@ -76,3 +81,23 @@ print(dstDt);
 
 dstDt = new Date(stdDt.getTime() + 2);
 print(dstDt);
+
+var testDates = [
+    [2016,  0, 1, 2, 3, 4, 5],
+    [2016,  1, 1, 2, 3, 4, 5],
+    [2016,  2, 1, 2, 3, 4, 5],
+    [2016,  3, 1, 2, 3, 4, 5],
+    [2016,  4, 1, 2, 3, 4, 5],
+    [2016,  5, 1, 2, 3, 4, 5],
+    [2016,  6, 1, 2, 3, 4, 5],
+    [2016,  7, 1, 2, 3, 4, 5],
+    [2016,  8, 1, 2, 3, 4, 5],
+    [2016,  9, 1, 2, 3, 4, 5],
+    [2016, 10, 1, 2, 3, 4, 5],
+    [2016, 11, 1, 2, 3, 4, 5],
+];
+
+for (var i = 0, iLen = testDates.length; i < iLen; ++i) {
+  var dt = createDt(testDates[i]);
+  print(dt);
+}
