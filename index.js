a4798bab670dedd5642cd4cebc74e71f92effc6c
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2016-07-02 13:08:38 -0600

    Print Info for Each Month of 2016.

--- index.js
+++ index.js
@@ -7,80 +7,83 @@ function createDt(dateParts) {
 }
 
 function print(dt) {
-  console.log(dt.getTimezoneOffset());
-  console.log(dt);
-  console.log(dt.toString());
-  console.log(dt.toISOString());
+  console.log('Timezone Offset: ' + dt.getTimezoneOffset());
+  console.log('toDateString: ' + dt.toDateString());
+  console.log('toGMTString: ' + dt.toGMTString());
+  console.log('toISOString: ' + dt.toISOString());
+  console.log('toString: ' + dt.toString());
+  console.log('toTimeString: ' + dt.toTimeString());
+  console.log('toUTCString: ' + dt.toUTCString());
 }
 
-stdDt = new Date(1976, 2, 14, 1, 59, 59, 999);
-print(stdDt);
+// stdDt = new Date(1976, 2, 14, 1, 59, 59, 999);
+// print(stdDt);
 
-dstDt = new Date(stdDt.getTime() + 1);
-print(dstDt);
+// dstDt = new Date(stdDt.getTime() + 1);
+// print(dstDt);
 
-dstDt = new Date(stdDt.getTime() + 2);
-print(dstDt);
-
-
-stdDt = new Date(1976, 2, 21, 1, 59, 59, 999);
-print(stdDt);
-
-dstDt = new Date(stdDt.getTime() + 1);
-print(dstDt);
-
-dstDt = new Date(stdDt.getTime() + 2);
-print(dstDt);
+// dstDt = new Date(stdDt.getTime() + 2);
+// print(dstDt);
 
 
-stdDt = new Date(1976, 2, 28, 1, 59, 59, 999);
-print(stdDt);
+// stdDt = new Date(1976, 2, 21, 1, 59, 59, 999);
+// print(stdDt);
 
-dstDt = new Date(stdDt.getTime() + 1);
-print(dstDt);
+// dstDt = new Date(stdDt.getTime() + 1);
+// print(dstDt);
 
-dstDt = new Date(stdDt.getTime() + 2);
-print(dstDt);
-
-
-stdDt = new Date(1976, 3, 4, 1, 59, 59, 999);
-print(stdDt);
-
-dstDt = new Date(stdDt.getTime() + 1);
-print(dstDt);
-
-dstDt = new Date(stdDt.getTime() + 2);
-print(dstDt);
+// dstDt = new Date(stdDt.getTime() + 2);
+// print(dstDt);
 
 
-stdDt = new Date(1976, 3, 11, 1, 59, 59, 999);
-print(stdDt);
+// stdDt = new Date(1976, 2, 28, 1, 59, 59, 999);
+// print(stdDt);
 
-dstDt = new Date(stdDt.getTime() + 1);
-print(dstDt);
+// dstDt = new Date(stdDt.getTime() + 1);
+// print(dstDt);
 
-dstDt = new Date(stdDt.getTime() + 2);
-print(dstDt);
-
-
-stdDt = new Date(1976, 3, 18, 1, 59, 59, 999);
-print(stdDt);
-
-dstDt = new Date(stdDt.getTime() + 1);
-print(dstDt);
-
-dstDt = new Date(stdDt.getTime() + 2);
-print(dstDt);
+// dstDt = new Date(stdDt.getTime() + 2);
+// print(dstDt);
 
 
-stdDt = new Date(1976, 3, 25, 1, 59, 59, 999);
-print(stdDt);
+// stdDt = new Date(1976, 3, 4, 1, 59, 59, 999);
+// print(stdDt);
 
-dstDt = new Date(stdDt.getTime() + 1);
-print(dstDt);
+// dstDt = new Date(stdDt.getTime() + 1);
+// print(dstDt);
 
-dstDt = new Date(stdDt.getTime() + 2);
-print(dstDt);
+// dstDt = new Date(stdDt.getTime() + 2);
+// print(dstDt);
+
+
+// stdDt = new Date(1976, 3, 11, 1, 59, 59, 999);
+// print(stdDt);
+
+// dstDt = new Date(stdDt.getTime() + 1);
+// print(dstDt);
+
+// dstDt = new Date(stdDt.getTime() + 2);
+// print(dstDt);
+
+
+// stdDt = new Date(1976, 3, 18, 1, 59, 59, 999);
+// print(stdDt);
+
+// dstDt = new Date(stdDt.getTime() + 1);
+// print(dstDt);
+
+// dstDt = new Date(stdDt.getTime() + 2);
+// print(dstDt);
+
+
+// stdDt = new Date(1976, 3, 25, 1, 59, 59, 999);
+// print(stdDt);
+
+// dstDt = new Date(stdDt.getTime() + 1);
+// print(dstDt);
+
+// dstDt = new Date(stdDt.getTime() + 2);
+// print(dstDt);
 
 var testDates = [
     [2016,  0, 1, 2, 3, 4, 5],
